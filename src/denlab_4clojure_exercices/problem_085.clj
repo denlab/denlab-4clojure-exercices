0b21e4f07de664cdbe485c72acabb9fda3ee8457
Denis Labaye <denis.labaye@gmail.com>
2012-01-29 16:49:40 +0100

    p85: final

--- src/denlab_4clojure_exercices/problem_085.clj
+++ src/denlab_4clojure_exercices/problem_085.clj
@@ -12,96 +12,11 @@
 ;; power set of a set x is the set of all subsets of x, including the
 ;; empty set and x itself.
 
-(defn children
-  [[g v]] (map
-           (fn [x] [
-                   (disj g x)
-                   (conj v x)])
-           g))
-
-(fact
-  (children [#{:a :b :c} #{} #{}]) => [[#{:b :c} #{:a} ]
-                                       [#{:a :b} #{:c}]
-                                       [#{:a :c} #{:b}]])
-
-(fact
-  (children [#{:b :c} #{:a}]) => [[#{:b} #{:a :c}]
-                                  [#{:c} #{:a :b}]]) 
-
-(defn ff
-  [{:keys [c p]}]  (let [d (mapcat children c)]
-                     {:c d
-                      :p (into p
-                               (map
-                                first c))}))
-
-(fact
-  (ff {:c [[#{:a :b} #{}]]
-       :p #{:prevs}}) => {:c [[#{:b} #{:a}] [#{:a} #{:b}]]
-                          :p #{:prevs #{:a :b}}})
-
-(defn end?
-  [gc] )
-
-(future-fact
-  (end? ))
-
-(defn gg
-  ([gc] (if (seq (:c gc))
-          (gg (ff gc))
-          (:p gc))))
-
-(defn f [x] (gg {:c [[x #{}]] :p #{}}))
-
-(fact
-  (f #{1 :a}) => #{#{1 :a} #{:a} #{} #{1}})
-(fact
-  (f #{}) => #{#{}})
-(fact
-  (f #{1 2 3}) =>    #{#{} #{1} #{2} #{3} #{1 2} #{1 3} #{2 3} #{1 2 3}})
-(future-fact
-  (count (f (into #{} (range 10)))) => 1024)
-
-(defn cnt
-  [s] (if s
-        (inc (cnt (next s)))
-        0))
-
-(fact
-  (cnt [:a :b]) => 2)
-
-(defn j
-  [s]
-  (if (second s)
-    (do  (set (doall (map #(do
-        
-                                        (conj (j (disj s %)) %))
-                                     s))))
-    (do  #{s})))
-
-(future-fact
-  (j #{:a :b :c #{}}) => #{#{}
-                       #{:a} #{:b} #{:c}
-                       #{:a :b}  #{:a :c}  #{:b :c}
-                       #{:a :b :c}})
-
-(defn combin
-  [n s]  (set (map #(conj % n) s)))
-
-(fact
-  (combin :a #{#{:b} #{:c}}) => #{#{:a :b} #{:a :c}})
-
-(defn p
-  [s]
-
-  (if (seq s)
-    (do 
-        (let [f (first s)
-              prev (p (disj s f))]
-          (into (conj prev #{f}) (combin f prev))))
-    (do 
-        #{#{}})))
-
+(def p
+  (comp set
+        (fn p [[f & r]] (if f (concat (p r) [#{f}] (map #(conj % f) (p r)))
+                           [#{}]))
+        seq))
 
 (fact
   (p #{:a :b :c}) => #{#{}
