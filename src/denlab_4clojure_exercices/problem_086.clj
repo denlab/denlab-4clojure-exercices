2b53d7d219bce34ab23c0607362296caa5e375cd
Denis Labaye <denis.labaye@gmail.com>
2012-01-30 19:39:45 +0100

    p86: init

--- src/denlab_4clojure_exercices/problem_086.clj
+++ src/denlab_4clojure_exercices/problem_086.clj
@@ -1,3 +1,29 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/86"}
     denlab-4clojure-exercices.problem-086
-  (:use [midje.sweet]))
+    (:use [midje.sweet]))
+
+;; Happy numbers
+ 
+;; Difficulty:	Medium
+;; Topics:	math
+
+
+;; Happy numbers are positive integers that follow a particular
+;; formula: take each individual digit, square it, and then sum the
+;; squares to get a new number. Repeat with the new number and
+;; eventually, you might get to a number whose squared sum is 1. This
+;; is a happy number. An unhappy number (or sad number) is one that
+;; loops endlessly. Write a function that determines if a number is
+;; happy or not.  
+
+(defn f
+  [n])
+
+(fact
+  (f 7) => true)
+(fact
+  (f 986543210) => true)
+(fact
+  (f 2) => false)
+(fact
+  (f 3) => false)
