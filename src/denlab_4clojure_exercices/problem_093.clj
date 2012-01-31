64591799a1beac4d9212d6d5ecb9053a279df64e
Denis Labaye <denis.labaye@gmail.com>
2012-01-31 07:54:08 +0100

    p93: init

--- src/denlab_4clojure_exercices/problem_093.clj
+++ src/denlab_4clojure_exercices/problem_093.clj
@@ -1,3 +1,25 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/93"}
     denlab-4clojure-exercices.problem-093
-  (:use [midje.sweet]))
+    (:use [midje.sweet]))
+
+;; Partially Flatten a Sequence
+ 
+;; Difficulty:	Medium
+;; Topics:	seqs
+
+;; Write a function which flattens any nested combination of
+;; sequential things (lists, vectors, etc.), but maintains the lowest
+;; level sequential items. The result should be a sequence of
+;; sequences with only one level of nesting.
+
+(def g
+  (fn [x]))
+
+(fact
+ (g [["Do"] ["Nothing"]]) => [["Do"] ["Nothing"]])
+
+(fact
+ (g [[[[:a :b]]] [[:c :d]] [:e :f]]) => [[:a :b] [:c :d] [:e :f]])
+
+(fact
+ (g '((1 2)((3 4)((((5 6))))))) => '((1 2)(3 4)(5 6)))
