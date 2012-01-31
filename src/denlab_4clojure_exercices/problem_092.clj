7601b0dfd9d6ec58dc4839e54302987ab9ff8cf9
Denis Labaye <denis.labaye@gmail.com>
2012-01-31 13:34:08 +0100

    p92: init

--- src/denlab_4clojure_exercices/problem_092.clj
+++ src/denlab_4clojure_exercices/problem_092.clj
@@ -1,3 +1,30 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/92"}
     denlab-4clojure-exercices.problem-092
-  (:use [midje.sweet]))
+    (:use [midje.sweet]))
+
+;; Read Roman numerals
+ 
+;; Difficulty:	Hard
+;; Topics:	strings math
+
+
+;; Roman numerals are easy to recognize, but not everyone knows all
+;; the rules necessary to work with them. Write a function to parse a
+;; Roman-numeral string and return the number it represents.
+
+;; You can assume that the input will be well-formed, in upper-case,
+;; and follow the subtractive principleM. You don't need to handle any
+;; numbers greater than MMMCMXCIX (3999), the largest number
+;; representable with ordinary letters.
+
+(defn g
+  [s])
+
+(fact
+ (g "XIV") => 14 )
+(fact
+ (g "DCCCXXVII") => 827 )
+(fact
+ (g "MMMCMXCIX") => 3999 )
+(fact
+ (g "XLVIII") => 48 )
--- src/denlab_4clojure_exercices/problem_104.clj
+++ src/denlab_4clojure_exercices/problem_104.clj
@@ -1,3 +1,38 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/104"}
     denlab-4clojure-exercices.problem-104
-  (:use [midje.sweet]))
+    (:use [midje.sweet]))
+
+;; Write Roman Numerals
+ 
+;; Difficulty:	Medium
+;; Topics:	strings math
+
+
+;; This is the inverse of Problem 92M, but much easier. Given an
+;; integer smaller than 4000, return the corresponding roman numeral
+;; in uppercase, adhering to the subtractive principleN.
+
+(defn g
+  [n])
+
+(future-fact
+ (g 1) => "I" )
+
+(future-fact
+ (g 30) => "XXX" )
+
+(future-fact
+ (g 4) => "IV" )
+
+(future-fact
+ (g 140) => "CXL" )
+
+(future-fact
+ (g 827) => "DCCCXXVII" )
+
+(future-fact
+ (g 3999) => "MMMCMXCIX" )
+
+(future-fact
+ (g 48) => "XLVIII" )
+
