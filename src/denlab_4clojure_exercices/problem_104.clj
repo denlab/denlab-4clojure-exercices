4d2cfbdcd5fce66d3b223449740dcae7c5029a66
Denis Labaye <denis.labaye@gmail.com>
2012-01-31 20:28:25 +0100

    p104: stable

--- src/denlab_4clojure_exercices/problem_104.clj
+++ src/denlab_4clojure_exercices/problem_104.clj
@@ -12,8 +12,32 @@
 ;; integer smaller than 4000, return the corresponding roman numeral
 ;; in uppercase, adhering to the subtractive principleN.
 
-(defn g
-  [n])
+
+(def m (zipmap "IVXLCDM" [1 5 10 50 100 500 1000]))
+
+
+(unfinished decomp)
+
+(defn transcod
+  [x] 
+  ({1  "I" , 4   "IV", 7     "VII" , 8   "VIII", 9    "IX", 20 "XX", 30 "XXX", 40 "XL",
+    90 "CL", 100 "C" , 800   "DCCC", 900 "CM"  , 3000 "MMM"}
+   x))
+
+(fact "transcod"
+  (transcod 3000) => "MMM"
+  (transcod 900) => "CM")
+
+(defn g 
+  [n] (reduce str (map transcod (decomp n))))
+
+(fact
+  (g :n) => "R1R2"
+  (provided
+    (decomp :n) => [:e1 :e2]
+    (transcod :e1) => "R1"
+    (transcod :e2) => "R2"))
+
 
 (future-fact
  (g 1) => "I" )
@@ -36,3 +60,6 @@
 (future-fact
  (g 48) => "XLVIII" )
 
+{1 "I", 2 "II", 3 "III", 4 "IV", 5 "V", "VI" , "VII", "VIII", "IV"}
+
+{10 "X", 11 "XI", 12 "XII", 13 "XIII", 14 "XIV", 15 "XV" , 16 "XVI", 17 "XVII", 18 "XVIII", 19 "IX"}
