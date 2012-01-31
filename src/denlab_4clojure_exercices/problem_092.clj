b86d8c794377f8ef594226c8de19ed538077b6bf
Denis Labaye <denis.labaye@gmail.com>
2012-01-31 17:48:10 +0100

    p92: tests green

--- src/denlab_4clojure_exercices/problem_092.clj
+++ src/denlab_4clojure_exercices/problem_092.clj
@@ -17,14 +17,40 @@
 ;; numbers greater than MMMCMXCIX (3999), the largest number
 ;; representable with ordinary letters.
 
+
+;; from wikipedia:
+
+;; I 	1
+;; V 	5
+;; X 	10
+;; L 	50
+;; C 	100
+;; D 	500
+;; M 	1000
+
+(unfinished sub?)
+
+(def transco
+  {\I 1 \V 5 \X 10 \L 50 \C 100 \D 500 \M 1000})
+
 (defn g
-  [s])
+  [s] (loop [[a b & r] (map transco s) m 0]
+        (do (prn "a" a "b" b)
+            (cond
+             (nil? a) (do (prn "->0") m)
+             (nil? b) (do (prn "->1") (+ m a)) ;; rest 1
+             (< a b ) (do (prn "->2" "r" r "m" m "b" b "a" a) (recur r (+ m (- b a)))) ;; rest 2, sub
+             :else    (do (prn "->3") (recur (cons b r) (+ m a))) ;; rest 2, !sub
+             ))))
 
 (fact
  (g "XIV") => 14 )
+
 (fact
  (g "DCCCXXVII") => 827 )
+
 (fact
  (g "MMMCMXCIX") => 3999 )
+
 (fact
  (g "XLVIII") => 48 )
