6f6ad1dc2bdabd947060da37dfe9e978871d5dfc
Denis Labaye <denis.labaye@gmail.com>
2012-02-14 08:20:47 +0100

    p114: stable

--- src/denlab_4clojure_exercices/problem_114.clj
+++ src/denlab_4clojure_exercices/problem_114.clj
@@ -1,8 +1,9 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/114"}
   denlab-4clojure-exercices.problem-114
   (:use [midje.sweet]
-        [clojure.repl :only [doc]]
-        [clojure.pprint :only [pprint]]))
+        [clojure
+         [repl :only [doc]]
+         [pprint :only [pprint]]]))
 
 ;; Global take-while
  
