e7d2052c17013344f9af731f3afc84b7c511ace6
Denis Labaye <denis.labaye@gmail.com>
2012-02-17 18:41:15 +0100

    tests green

--- src/denlab_4clojure_exercices/problem_131.clj
+++ src/denlab_4clojure_exercices/problem_131.clj
@@ -20,41 +20,49 @@
         seq))
 
 (defn g
-  [& s] (map (fn [x]
-               (map #(reduce + %)
-                    (remove empty? x)))
-             (map p s)))
+  [& s] (seq (apply clojure.set/intersection
+                    (map (fn [x]
+                           (set (map #(reduce + %)
+                                     (remove empty? x))))
+                         (map p s)))))
 
 (fact
   (g #{-1 1 99}
              #{-2 2 888}
-             #{-3 3 7777}) => true  ) ; ex. all sets have a subset which sums to zero
-(future-fact
+             #{-3 3 7777}) => truthy  ) ; ex. all sets have a subset which sums to zero
+(fact
   (g #{1}
      #{2}
      #{3}
-     #{4}) => false )
-(future-fact
-  (g #{1}) => true  )
-(future-fact
+     #{4}) => falsey )
+
+(fact
+  (g #{1}) => truthy  )
+
+(fact
   (g #{1 -3 51 9}
      #{0}
-     #{9 2 81 33}) => false )
-(future-fact
+     #{9 2 81 33}) => falsey )
+
+(fact
   (g #{1 3 5}
      #{9 11 4}
      #{-3 12 3}
-     #{-3 4 -2 10}) => true  )
-(future-fact
+     #{-3 4 -2 10}) => truthy  )
+
+(fact
   (g #{-1 -2 -3 -4 -5 -6}
-     #{1 2 3 4 5 6 7 8 9}) => false )
-(future-fact
+     #{1 2 3 4 5 6 7 8 9}) => falsey )
+
+(fact
   (g #{1 3 5 7}
-     #{2 4 6 8}) => true  )
-(future-fact
+     #{2 4 6 8}) => truthy  )
+
+(fact
   (g #{-1 3 -5 7 -9 11 -13 15}
      #{1 -3 5 -7 9 -11 13 -15}
-     #{1 -1 2 -2 4 -4 8 -8}) => true  )
-(future-fact
+     #{1 -1 2 -2 4 -4 8 -8}) => truthy  )
+
+(fact
   (g #{-10 9 -8 7 -6 5 -4 3 -2 1}
-     #{10 -9 8 -7 6 -5 4 -3 2 -1}) => true  )
+     #{10 -9 8 -7 6 -5 4 -3 2 -1}) => truthy  )
