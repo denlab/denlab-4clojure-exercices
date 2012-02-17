c90b377cc307f44dc74989ecd54df079375f358c
Denis Labaye <denis.labaye@gmail.com>
2012-02-17 17:23:49 +0100

    p121: stable

--- src/denlab_4clojure_exercices/problem_121.clj
+++ src/denlab_4clojure_exercices/problem_121.clj
@@ -1,3 +1,34 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/121"}
-    denlab-4clojure-exercices.problem-121
+  denlab-4clojure-exercices.problem-121
   (:use [midje.sweet]))
+ 
+;; Difficulty:	Medium
+;; Topics:	functions
+
+
+;; Given a mathematical formula in prefix notation, return a function
+;; that calculates the value of the formula. The formula can contain
+;; nested calculations using the four basic mathematical operators,
+;; numeric constants, and symbols representing variables. The returned
+;; function has to accept a single parameter containing the map of
+;; variable names to their values.
+
+(defn g
+  [x] identity)
+
+(fact
+  ((g '(/ a b))
+      '{b 8 a 16}) => 2)
+(fact
+  ((g '(+ a b 2))
+      '{a 2 b 4}) => 8)
+(fact
+  (map (g '(* (+ 2 a)
+                  (- 10 b)))
+            '[{a 1 b 8}
+              {b 5 a -2}
+              {a 2 b 11}]) => [6 0 -4])
+(fact
+  ((g '(/ (+ x 2)
+              (* 3 (+ y 1))))
+      '{x 4 y 1}) => 1)
