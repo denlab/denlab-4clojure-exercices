ca114f8b5c02e5daee5887663b65ca0715a2a51e
Denis Labaye <denis.labaye@gmail.com>
2012-02-18 19:10:17 +0100

    p132: stable

--- src/denlab_4clojure_exercices/problem_132.clj
+++ src/denlab_4clojure_exercices/problem_132.clj
@@ -1,6 +1,7 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/132"}
     denlab-4clojure-exercices.problem-132
-    (:use [midje.sweet]))
+    (:use [clojure.pprint :only [pprint]]
+     [midje.sweet]))
 
  
 ;; Difficulty:	Medium
@@ -11,17 +12,39 @@
 ;; a collection; and returns a new collection where the value is
 ;; inserted between every two items that satisfy the predicate.
 
-(def g
-  (fn [p v s] (if (seq s)
-               (cons (first s)
-                     (reduce (fn [r [o [b a]]] (if (p b a)
-                                                (conj r v o)
-                                                (conj r o)))
-                             []
-                             (map vector
-                                  (rest s)
-                                  (partition 2 1 s))))
-               [])))
+(defn- pairify
+  [s] (map vector
+           (rest s)
+           (partition 2 1 s)))
+
+(defn- pairify2
+  [s] (partition 2 1 s))
+
+(def s (pairify2  [1 6 7 4 3]))
+(def i (iterate (fn [[[[a b] & r] o]] (if (< a b)
+                                       [(if (nil? r) :end r)  [a :less b]]
+                                       [(if (nil? r) :end r)  [a       b]]))
+        [s []]))
+
+
+(defn g
+  [p v s] (flatten (map second
+                        (take-while #(not (nil? (first %)))
+                                    (iterate (fn [[[[a b] & r] o]] (if (p a b)
+                                                                    [r  [v b]]
+                                                                    [r  [  b]]))
+                                             [(pairify2 s) []])))))
+
+(defn g
+  [p v s] (if (seq s)
+            (cons (first s)
+                  (flatten (map (fn [x [b a]] (if (p b a)
+                                               [v a]
+                                               [a]))
+                                s (partition 2 1 s))))
+            []))
+
+
 
 (fact
   (g < :less [1 6 7 4 3]) => '(1 :less 6 :less 7 4 3) )
