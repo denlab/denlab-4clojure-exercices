1e02842e91d05bbfb3b61e72a2d8274675fd4fe9
Denis Labaye <denis.labaye@gmail.com>
2012-02-19 13:47:50 +0100

    p144: init

--- src/denlab_4clojure_exercices/problem_144.clj
+++ src/denlab_4clojure_exercices/problem_144.clj
@@ -1,3 +1,23 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/144"}
     denlab-4clojure-exercices.problem-144
-  (:use [midje.sweet]))
+    (:use [midje.sweet]))
+
+;; Oscilrate
+ 
+;; Difficulty:	Medium
+;; Topics:	sequences
+
+;; Write an oscillating iterate: a function that takes an initial
+;; value and a variable number of functions. It should return a lazy
+;; sequence of the functions applied to the value in order, restarting
+;; from the first function after it hits the end.
+
+(defn g
+  [& f])
+
+(fact
+  (take 3 (g 3.14 int double)) =>  [3.14 3 3.0])
+(fact
+  (take 5 (g 3 #(- % 3) #(+ 5 %))) =>  [3 0 5 2 7])
+(fact
+  (take 12 (g 0 inc dec inc dec inc)) =>  [0 1 0 1 0 1 2 1 2 1 2 3])
