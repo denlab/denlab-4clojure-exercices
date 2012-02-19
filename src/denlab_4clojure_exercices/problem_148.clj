d37a4d2929fdf2eb2963049fabf7b07ea5d75af0
Denis Labaye <denis.labaye@gmail.com>
2012-02-19 15:01:53 +0100

    p148: stable

--- src/denlab_4clojure_exercices/problem_148.clj
+++ src/denlab_4clojure_exercices/problem_148.clj
@@ -1,6 +1,6 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/148"}
     denlab-4clojure-exercices.problem-148
-    (:use [midje.sweet]))
+    (:use [clojure.pprint :only [pprint]] [midje.sweet]))
 
 ;; The Big Divide
  
@@ -16,15 +16,32 @@
 ;; Note: Some test cases have a very large n, so the most obvious
 ;; solution will exceed the time limit.
 
+(defn h
+  [n a] (let [m (quot (dec n) a)]
+          (* m (inc m) 1/2 a)))
+
+(fact
+  (h 10 3) =>  18 )
+
+(fact
+  (h 10 2) => 20)
+
 (defn g
-  [n a b] (reduce + (concat (take-while #(< % n) (iterate #(+ a %) a))
-                            (take-while #(< % n) (iterate #(+ b %) b)))))
+  [n a b] (+ (h n a) (h n b) (- (h n (* a b)))))
+
+(defn exp
+  [n a b] (map #(vector % (if (or (some (fn [x] (zero? (rem % x)))
+                                        [a b]))
+                            :yes))
+               (iterate inc 1)))
 
 (fact
   (g 10 3 5)                               =>  23 )
 (fact
-  (g 3 17 11)                              =>  0 )
+  (g 16 3 5)                               =>  60)
 (fact
+  (g 3 17 11)                              =>  0 )
+(future-fact
   (g 1000 3 5)                             =>  233168 )
 (future-fact
   (str (g 100000000 3 5))                  =>  "2333333316666668" )
