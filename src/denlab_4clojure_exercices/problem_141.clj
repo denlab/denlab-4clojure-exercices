81bbe5b72628ef6a9ce3017d9438f29f4ebf2013
Denis Labaye <denis.labaye@gmail.com>
2012-02-19 13:02:04 +0100

    p141: init

--- src/denlab_4clojure_exercices/problem_141.clj
+++ src/denlab_4clojure_exercices/problem_141.clj
@@ -1,3 +1,45 @@
 (ns ^{:doc "Problem: http://www.4clojure.com/problem/141"}
     denlab-4clojure-exercices.problem-141
-  (:use [midje.sweet]))
+    (:use [midje.sweet]))
+
+;; Tricky card games
+ 
+;; Difficulty:	Medium
+;; Topics:	game cards
+
+
+;; In trick-taking card games such as bridge, spades, or hearts, cards
+;; are played in groups known as "tricks" - each player plays a single
+;; card, in order; the first player is said to "lead" to the
+;; trick. After all players have played, one card is said to have
+;; "won" the trick. How the winner is determined will vary by game,
+;; but generally the winner is the highest card played in the suit
+;; that was led. Sometimes (again varying by game), a particular suit
+;; will be designated "trump", meaning that its cards are more
+;; powerful than any others: if there is a trump suit, and any trumps
+;; are played, then the highest trump wins regardless of what was led.
+
+;; Your goal is to devise a function that can determine which of a
+;; number of cards has won a trick. You should accept a trump suit,
+;; and return a function winner. Winner will be called on a sequence
+;; of cards, and should return the one which wins the trick. Cards
+;; will be represented in the format returned by Problem 128,
+;; Recognize Playing Cards: a hash-map of :suit and a numeric
+;; :rank. Cards with a larger rank are stronger.
+
+(defn g
+  [t] identity)
+
+(fact
+  (let [notrump (g nil)]
+    (notrump [{:suit :club :rank 4}
+              {:suit :club :rank 9}]) => {:suit :club :rank 9}
+              
+    (notrump [{:suit :spade :rank 2}
+              {:suit :club :rank 10}]) => {:suit :spade :rank 2}))
+(fact
+  ((g :club) [{:suit :spade :rank 2}
+              {:suit :club :rank 10}]) =>  {:suit :club :rank 10} )
+(fact
+  ((g :heart) [{:suit :heart :rank 6} {:suit :heart :rank 8}
+               {:suit :diamond :rank 10} {:suit :heart :rank 4}]) =>  {:suit :heart :rank 8})
