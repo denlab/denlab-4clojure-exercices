53131e5f50390b081d3b1e34a9435dc82d70fba7
Denis Labaye <denis.labaye@gmail.com>
2012-06-03 16:17:09 +0200

    added clojure.tools.trace

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject denlab-4clojure-exercices "1.0.0-SNAPSHOT"
   :description "https://github.com/denlab exercices of http://www.4clojure.com"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[midje "1.3.1"]
-                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
+  :dev-dependencies [[midje                   "1.3.1"]
+                     [org.clojure/tools.trace "0.7.3"]
+                     [com.intelie/lazytest    "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
