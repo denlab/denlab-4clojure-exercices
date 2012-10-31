4956e2ac47bf070c1bc4a4829ac8560b6a517a51
Denis Labaye <denis.labaye@gmail.com>
2012-10-31 10:14:20 +0100

    migrated project.clj to lein2

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject denlab-4clojure-exercices "1.0.0-SNAPSHOT"
-  :description "https://github.com/denlab exercices of http://www.4clojure.com"
+  :min-lein-version "2.0.0"
+  :profiles {:dev
+             {:dependencies
+              [[midje                   "1.3.1"]
+               [org.clojure/tools.trace "0.7.3"]
+               [com.intelie/lazytest    "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]
+               [org.clojure/tools.trace "0.7.3"]]}}
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[midje                   "1.3.1"]
-                     [org.clojure/tools.trace "0.7.3"]
-                     [com.intelie/lazytest    "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
+  :description "FIXME: write description")
