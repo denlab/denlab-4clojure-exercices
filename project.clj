83243e3b21026a9621d527292d999fe7151247f2
Denis Labaye <denis.labaye@gmail.com>
2013-04-13 14:56:44 +0200

    upgrade to clojure 1.4.0 (the same as 4clojure)

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
               [[midje                   "1.3.1"]
                [org.clojure/tools.trace "0.7.3"]
                [com.intelie/lazytest    "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]
-               [org.clojure/tools.trace "0.7.3"]]}}
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+               [org.clojure/tools.trace "0.7.3"]
+               [table "0.3.2"]]}}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :description "FIXME: write description")
